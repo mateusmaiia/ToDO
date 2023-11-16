edf793d4687a0e683334b10ea7fbce8e4456e826
maia <matteusmaia889@gmail.com>
2023-11-16 13:36:58 -0300

    base css app

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ function App() {
   
 
   return (
-    <>
-     <h1>Hello Maia</h1>
-    </>
+    <section id="app" className="container">
+      <header>
+        <h1 className="title">todo</h1>
+      </header>
+      <section className="main">
+        <input type="text" className="new-todo"/>
+      </section>
+    </section>
   )
 }
 
