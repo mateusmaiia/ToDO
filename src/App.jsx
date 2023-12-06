7178ac415fa0d651ab504fa3af9145cb0dc9569a
maia <matteusmaia889@gmail.com>
2023-12-06 15:51:19 -0300

    feito o comportamento do input de alocar o valor em um estado, função de onKeyDown para quando der enter ou Esc

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
+import './app.css'
+import { useState } from 'react'
 
 function App() {
-  
+  const [value, setValue] = useState('')
+
+
+  const ENTER_KEY = 13
+  const ESCAPE_KEY = 27
+/**
+  A função verifica se a tecla pressionada é a tecla Enter e se o valor de entrada não está vazio, ela
+ *alerta o valor; caso contrário, se a tecla pressionada for a tecla Escape, o valor de entrada será limpo.
+ */
+  function onKeyDown(event){
+    if(event.which === ENTER_KEY && value.length > 0){
+      alert(value)
+      setValue('')
+    }else if(event.which === ESCAPE_KEY){
+      setValue('')
+    }
+  }
 
   return (
     <section id="app" className="container">
@@ -8,7 +26,14 @@ function App() {
         <h1 className="title">todo</h1>
       </header>
       <section className="main">
-        <input type="text" className="new-todo"/>
+        <input 
+          type="text" 
+          className="new-todo"
+          placeholder='O que precisa ser feito?'
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          onKeyDown={onKeyDown}
+        />
       </section>
     </section>
   )
